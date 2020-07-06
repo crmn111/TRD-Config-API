949bff253f698b634f2753045f21a9828f67b961
Dmytro Savchenko <login.was.here@gmail.com>
2020-07-06 20:02:26 +0300

    Add HOST param

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ require('dotenv').config()
 
 const app = require('./app')
 const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || 'localhost'
 
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
+app.listen(PORT, HOST, () => console.log(`App listening at http://${HOST}:${PORT}`))
